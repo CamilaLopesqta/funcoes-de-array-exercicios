39eed1ce51068c0f9c1cdc9e624862726f4ba66c
Camila Lopes <camilalopesqta@gmail.com>
2023-06-07 21:28:51 -0300

    exercícios concluídos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,34 @@
+//EXERCÍCIO 1
+
 const infosPessoa = {
     nome: "astrodev",
     profissao: "Dev das estrelas",
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+const caixaAlta = (objeto) => {
+    let novoObjeto = {}
+    for (let i in objeto){
+        novoObjeto[i] = objeto[i].toUpperCase()
+    }
+    return novoObjeto
+    }
+    console.log(caixaAlta(infosPessoa))
+
+const textoCorrido = (retornarValores) => {
+    let textoCorrido1 = ``
+    for (let i in retornarValores){
+        textoCorrido1 += `O valor da propriedade "${i}" é "${retornarValores[i]}".\n`
+    }
+    return textoCorrido1
+}
+console.log(textoCorrido(infosPessoa))
+
+function objetoDisponibilizado (retornarValores, callback){
+    const objDisp = callback(retornarValores)
+    return objDisp
+}
+
+console.log(objetoDisponibilizado(infosPessoa,caixaAlta))
+console.log(objetoDisponibilizado(infosPessoa,textoCorrido));
